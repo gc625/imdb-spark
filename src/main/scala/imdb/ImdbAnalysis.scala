9b0a50ce14e5292f411233e0fd4fddbff5235236
gc625 <gabrielchan625@gmail.com>
2021-11-22 23:10:32 +0000

    save

--- src/main/scala/imdb/ImdbAnalysis.scala
+++ src/main/scala/imdb/ImdbAnalysis.scala
@@ -1,5 +1,6 @@
 package imdb
 
+import math._
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
@@ -22,27 +23,120 @@ object ImdbAnalysis {
 
 
   // Hint: use a combination of `ImdbData.titleBasicsPath` and `ImdbData.parseTitleBasics`
-  val titleBasicsRDD: RDD[TitleBasics] = sc.textFile(ImdbData.titleBasicsPath).map(ImdbData.parseTitleBasics)
+  val titleBasicsRDD: RDD[TitleBasics] = sc.textFile(ImdbData.titleBasicsPath).map(ImdbData.parseTitleBasics(_))
 
   // Hint: use a combination of `ImdbData.titleRatingsPath` and `ImdbData.parseTitleRatings`
-  val titleRatingsRDD: RDD[TitleRatings] = ???
+  val titleRatingsRDD: RDD[TitleRatings] = sc.textFile(ImdbData.titleRatingsPath).map(ImdbData.parseTitleRatings(_))
 
   // Hint: use a combination of `ImdbData.titleCrewPath` and `ImdbData.parseTitleCrew`
-  val titleCrewRDD: RDD[TitleCrew] = ???
+  val titleCrewRDD: RDD[TitleCrew] = sc.textFile(ImdbData.titleCrewPath).map(ImdbData.parseTitleCrew(_))
 
   // Hint: use a combination of `ImdbData.nameBasicsPath` and `ImdbData.parseNameBasics`
-  val nameBasicsRDD: RDD[NameBasics] = ???
+  val nameBasicsRDD: RDD[NameBasics] = sc.textFile(ImdbData.nameBasicsPath).map(ImdbData.parseNameBasics(_))
 
-  println("bruh")
-  titleBasicsRDD.map(println)
+  
+
+
 
   def task1(rdd: RDD[TitleBasics]): RDD[(Float, Int, Int, String)] = {
-  	println("in here?")
-    ???
+  	val filteredRDD = rdd
+  	.filter(x => x.runtimeMinutes.getOrElse(0) != 0 && x.genres.getOrElse(List())!= List())
+  	
+  	// val filteredRDD = rdd
+  	// .filter(x => x.runtimeMinutes.getOrElse(0) != 0 && x.genres.getOrElse(List())!= List())
+  	
+  	val genreRuntime = filteredRDD
+  	.flatMap(x => x.genres.getOrElse(List()).map(y=> (y,x.runtimeMinutes.getOrElse(-1))))
+  	
+  	// genreRuntime.take(5).foreach(println)
+  	// val test =genreRuntime.flatMap(x => x)
+  	// test.take(5).foreach(println)
+  	// println(genreRuntime)
+
+  	// (sumTime,numMovies), minTime,MaxTime
+  	type GenreCollector = (Int,Int,Int,Int)
+  	// (sumTime,numMovies), minTime,MaxTime , GENRE
+  	type GenreData = (String,(Int,Int,Int,Int))
+
+
+  	val createGenreCombiner = (runtime:Int) => (runtime,1,runtime,runtime)
+
+  	
+  	val genreCombiner = (collector: GenreCollector, runtime: Int) => {
+  		val (cumTime,numMovies,minTime,maxTime) = collector
+  		(cumTime+runtime,numMovies+1,math.min(runtime,minTime),math.max(runtime,maxTime))
+  	}
+
+  	val genreMerger = (c1: GenreCollector, c2: GenreCollector) => {
+  		val (cumTime1,numMovies1,minTime1,maxTime1) = c1
+  		val (cumTime2,numMovies2,minTime2,maxTime2) = c2
+
+  		(cumTime1+cumTime2,numMovies1+numMovies2,math.min(minTime1,minTime2),math.max(maxTime1,maxTime2))
+  	}
+
+
+  	val results: RDD[(GenreData)] = genreRuntime.combineByKey(createGenreCombiner,genreCombiner,genreMerger)
+
+  	// println(results)
+
+  	val formatResults = (aGenre: GenreData) => {
+  		val (genre, (cumTime,numMovies,minTime,maxTime)) = aGenre
+
+  		(cumTime.toFloat/numMovies,minTime,maxTime,genre)
+  	}
+
+  	val formattedResults = results.map(formatResults)
+
+  	// print(formattedResults)	
+
+  	// formattedResults.foreach(println)
+  	
+    val data = List((1.23f, 1,1,"bruh"))
+    val rdd2 = sc.parallelize(data)
+    // println(rdd2)
+    // return rdd2
+    return formattedResults
   }
 
   def task2(l1: RDD[TitleBasics], l2: RDD[TitleRatings]): RDD[String] = {
-    ???
+    // val filtered  =  l1.filter(_.runtimeMinutes.getOrElse(-1)>= 0) 
+
+ 	val filteredRating = l2.filter(x => (x.numVotes >= 500000 && x.averageRating >= 7.5)).map( x => (x.tconst,true))
+ 	// val filteredRating = l2.map(x => (x.tconst,x.numVotes >= 500000 && x.averageRating >= 7.5))
+ 	// println(filteredRating)
+ 	// println(filteredRating.lookup("t01"))
+ 	 val filteredTitles = l1.filter( x => (x.runtimeMinutes.getOrElse(-1)>=0 && (1990 <= x.startYear.getOrElse(0) && x.startYear.getOrElse(0) <= 2018) && x.titleType.getOrElse("") == "movie"))
+ 	 .map(x => (x.tconst,x.primaryTitle.getOrElse("")))
+ 	// val filteredTitles = l1.map(x => (x.tconst,(x.runtimeMinutes.getOrElse(-1)>=0 && (1990 <= x.startYear.getOrElse(0) && x.startYear.getOrElse(0) <= 2018) && x.titleType.getOrElse("") == "movie")))
+
+	val joined = filteredTitles.join(filteredRating).map(x => x._2._1)
+
+	return joined
+	// joined.foreach(println)
+	// return joined.map(x => )
+
+
+	// all_valid.foreach(println) 	
+ 	// println
+ 	// filteredRating.foreach(println)
+ 	// println(filteredRating("t01"))
+ 	// val filteredTitles = l1.filter( x => filteredRating.(x.tconst)
+ 		// && (x.runtimeMinutes.getOrElse(-1)>=0 && (1990 <= x.startYear.getOrElse(0) && x.startYear.getOrElse(0) <= 2018) && x.titleType.getOrElse("") == "movie"))
+
+    // O(n)?
+    // val filteredTitles = l1
+    // .filter(x => (x.runtimeMinutes.getOrElse(-1)>=0 && (1990 <= x.startYear.getOrElse(0) && x.startYear.getOrElse(0) <= 2018) && x.titleType.getOrElse("") == "movie"))
+    // 
+    // O(1)? 
+    // val moviesFiltered = filteredTitles.filter(x => filteredRating(x.tconst)).map(x => x.primaryTitle.getOrElse(""))
+    // filteredTitles.foreach(println)
+    // val data = List("bruh")
+    // val rdd2 = sc.parallelize(data)
+    
+    // return rdd2
+
+
+
   }
 
   def task3(l1: RDD[TitleBasics], l2: RDD[TitleRatings]): RDD[(Int, String, String)] = {
